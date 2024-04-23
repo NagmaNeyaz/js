3148d0f8b84532c1ab3f6549d6978e1990c3816f
NagmaNeyaz <156667894+NagmaNeyaz@users.noreply.github.com>
2024-04-23 12:55:22 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+console.log("2" > 1);  // comparision in two different datatypes
+console.log("02" > 1);
+
+console.log(null > 0); //f
+console.log(null == 0);//f
+console.log(null >= 0);//f
+
+
+console.log(undefined == 0); //in tino cases it will give false
+
+//===
+//strict conversion  1st two == check conversions last single = check is it same datatype or not
+console.log("2" === 2);
+console.log(2 === 2);
