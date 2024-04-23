28d91655cf719925ee2f3b1cfd114577c06c9ed9
NagmaNeyaz <156667894+NagmaNeyaz@users.noreply.github.com>
2024-04-23 13:39:45 +0000

    DataType_Summary.js

---
+++ 01_basics/dataTypes_Summary.js
@@ -0,0 +1,23 @@
+//primitive datatype
+// 7 types => String, Number, Boolean, null, undefined, Symbol
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId );
+
+const bigNumber = 23456789087654321n
+
+ //non primitove
+ //Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj=
+ {
+    name: "Nagma",
+    age:21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
