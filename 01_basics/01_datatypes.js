a4cdbd4dbdfbbbceba8fa83480015089a90d4f72
NagmaNeyaz <156667894+NagmaNeyaz@users.noreply.github.com>
2024-04-23 04:05:55 +0000

    DataTypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as a newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+
+let name = "Nagma"
+let age = 18
+let isloggedIn = false
+
+//number => 2 to power 53  (range)
+//bigint => dor very large number
+//string =>""
+//boolean => true/false
+//null => standalone  value
+//undefined =>
+//symbol => uniqueness
+
+//object => will discuss gurther
+
+
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
+
