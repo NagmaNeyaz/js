92ab0e52b7b462701d261c85fca26dc81c1dedb4
NagmaNeyaz <156667894+NagmaNeyaz@users.noreply.github.com>
2024-04-27 14:27:20 +0000

    chnges

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,3 +31,40 @@
 
      //console.log(loginUserMessage("Nagma"));
      console.log(loginUserMessage());//if we not pass any value than o/p will be undefined
+
+     //shopping cart
+
+     function calculateCartPrice(val1,val2,...num1)
+     {
+        console.log(val1);
+        return num1
+     }
+     console.log(calculateCartPrice(200,3,400,556,432));
+
+//how to pass object in functions
+const user = {
+    username: "Nagma",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username}, and price is ${anyObject.price}`);
+}
+//handleObject(user)
+
+//direct pass object
+handleObject({
+    username: "Nagma",
+    price: 399 
+})
+
+//how to pass Array in functions
+const myNewArray = [200,786,232,344]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,786,232,344]));
